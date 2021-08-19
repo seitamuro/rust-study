8cebacb39b9710e9214ec3b9261354c877db5d99
seitamuro <seitamuro0210@gmail.com>
2021-08-19 09:38:50 +0900

    add Deref<T>

--- src/bin/smartpointer/main.rs
+++ src/bin/smartpointer/main.rs
@@ -1,5 +1,8 @@
 mod list;
+mod mybox;
+
 use list::List::{Cons, Nil};
+use mybox::MyBox;
 
 fn main() {
     // Box型｡ヒープデータへのポインタ｡
@@ -12,4 +15,17 @@ fn main() {
         Box::new(Cons(4,
         Box::new(Nil))))))));
     println!("{:?}", conslist);
+
+    // Deref｡参照外し演算子の動作を定義している｡
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    let x = 5;
+    let y = MyBox::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }
